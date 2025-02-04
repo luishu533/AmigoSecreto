a735a0694d53f038b4dbd2e6215c9375dc6b7b06
Luis Huanca <130894533+luishu533@users.noreply.github.com>
2025-02-04 15:47:28 -0500

    implementación de JavaScript
    se creó la lista.
    se creó la función para agregar amigos.
    se creó la función para actualizar la lista de amigos.
    se creó una función extra para poner en mayúsculas el nombre y apellido en el caso de que las persones ingresen su nombre y apellido en minúsculas.

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo.trim() === "") {;
+        alert("Por favor, inserte un nombre")
+        return;
+    }
+    nombreAmigo = capitalizarNombre(nombreAmigo);
+    amigos.push(nombreAmigo);
+    document.getElementById("amigo").value = "";
+    actualizarListaAmigos();
+}
+
+function capitalizarNombre(nombre) {
+    nombre = nombre.toLowerCase();
+    let palabras = nombre.split(" ");
+    palabras = palabras.map(palabra => {
+        if (palabra.length > 0) {
+            return palabra[0].toUpperCase() + palabra.slice(1);
+        } else {
+            return palabra;
+        }
+    });
+    return palabras.join(" ");    
+}
+
+function actualizarListaAmigos() {
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+    amigos.forEach(function (amigo) {
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
